58f1829716face12b157974cf540e1b0b8212af5
gpaul988 <gpaul988@gmail.com>
2023-06-22 21:58:17 -0700

    push correction

--- push.c
+++ push.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 /**
-* f_push - Include node to stack
+* f_push – Include new node to stack
 * @head: Beginning of stack
 * @counter: Number of lines
-* Return: no return
+* Return: Void
 */
 void f_push(stack_t **head, unsigned int counter)
 {
@@ -16,25 +16,19 @@ j++;
 for (; bus.arg[j] != '\0'; j++)
 {
 if (bus.arg[j] > 57 || bus.arg[j] < 48)
-flag = 1;
-}
+flag = 1; }
 if (flag == 1)
-{
-fprintf(stderr, "L%d: usage: push integer\n", counter);
+{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 fclose(bus.file);
 free(bus.content);
 free_stack(*head);
-exit(EXIT_FAILURE);
-}
-}
+exit(EXIT_FAILURE); }}
 else
-{
-fprintf(stderr, "L%d: usage: push integer\n", counter);
+{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 fclose(bus.file);
 free(bus.content);
 free_stack(*head);
-exit(EXIT_FAILURE);
-}
+exit(EXIT_FAILURE); }
 n = atoi(bus.arg);
 if (bus.lifi == 0)
 addnode(head, n);
